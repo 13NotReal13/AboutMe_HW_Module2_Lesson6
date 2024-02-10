3d4568308f80eb4fd3a9b4bf003e2253fc3ece4d
Ivan Semikin <i.semikin.94@icloud.com>
2024-02-10 19:31:05 +0100

    Initial Commit

---
+++ AboutMe_HW_Module2_Lesson6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe_HW_Module2_Lesson6
+//
+//  Created by Иван Семикин on 10/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
